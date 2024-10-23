fb09938e24640fc67b6722d8c476c6da0d7c48eb
Linda Liu <linda20138@yahoo.com.au>
2024-10-24 10:37:59 +1100

    Add image retriever

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
 import time
 import faiss
+import uuid
+
 from openai import OpenAI
 import gradio as gr
 from pathlib import Path
@@ -8,11 +10,12 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.messages import HumanMessage
 
 # from langchain import OpenAI, CoversationChain, LLMChain
-from langchain.chains.conversation.memory import ConversationBufferWindowMemory 
+from langchain.retrievers.multi_vector import MultiVectorRetriever
 from langchain_core.output_parsers import StrOutputParser
+from langchain_core.documents import Document
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import CharacterTextSplitter, RecursiveCharacterTextSplitter
-from langchain_core.vectorstores import InMemoryVectorStore
+from langchain.storage import InMemoryStore
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnablePassthrough
 from langchain_community.vectorstores import FAISS
@@ -20,54 +23,145 @@ from langchain_openai import OpenAIEmbeddings
 from langchain_openai import ChatOpenAI
 from langchain_community.document_loaders import UnstructuredPDFLoader
 
-from pdf2image import convert_from_path
+import base64
+import io
+
+import fitz
+from langchain_core.messages import HumanMessage
+from PIL import Image
+# from IPython.display import Image as IPImage
+# from IPython.display import display
 
 
 from dotenv import load_dotenv, find_dotenv
 
 _ = load_dotenv(find_dotenv()) # read local .env file
 
-OPENAI_MODEL=os.environ.get("OPENAI_MODEL")
+OPENAI_MODEL=os.environ.get("OPENAI_MODEL") # gpt-4o-mini
 print("model: {}".format(OPENAI_MODEL))
 
 retriever = ""
 pages = []
 
-def define_retriever(pages):
-    print('define retriever')
-     # text splitter
-    text_splitter = RecursiveCharacterTextSplitter(
-        # shows how to seperate
-        separators=["\n\n", "\n", " ", ""],
-        # Shows the document token length
-        chunk_size=1000,
-        # How much overlap should exist between documents
-        chunk_overlap=200,
-        # How to measure length
-        length_function=len
-        )
+def create_multi_vector_retriever(
+    vectorstore, image_summaries, images
+):
+    """
+    Create retriever that indexes summaries, but returns raw images or texts
+    """
 
-    # Applying the splitter
-    docs = text_splitter.split_documents(pages)
+    # Initialize the storage layer
+    store = InMemoryStore()
+    id_key = "doc_id"
 
-    # uses OpenAI embeddings to build a retriever
-    embeddings = OpenAIEmbeddings(api_key=os.environ.get("OPENAI_API_KEY"))
+    # Create the multi-vector retriever
+    retriever = MultiVectorRetriever(
+        vectorstore=vectorstore,
+        docstore=store,
+        id_key=id_key,
+    )
 
-    # Creates the document retriever using docs and embeddings
-    db = FAISS.from_documents(docs, embeddings)
+    # Helper function to add documents to the vectorstore and docstore
+    def add_documents(retriever, doc_summaries, doc_contents):
+        doc_ids = [str(uuid.uuid4()) for _ in doc_contents]
+        summary_docs = [
+            Document(page_content=s, metadata={id_key: doc_ids[i]})
+            for i, s in enumerate(doc_summaries)
+        ]
+        retriever.vectorstore.add_documents(summary_docs)
+        retriever.docstore.mset(list(zip(doc_ids, doc_contents)))
 
-    # Asking the retriever to do similarity search based on Query
-    # answer = db.similarity_search(text)
+    # Check that image_summaries is not empty before adding
+    if image_summaries:
+        add_documents(retriever, image_summaries, images)
 
-    # Building the retriever
-    return db.as_retriever(search_type="mmr", search_kwargs={'k': 6})
-    
+    return retriever
 
-def ask_ai(text, retriever):
+
+# The vectorstore to use to index the summaries
+vectorstore = Chroma(
+    collection_name="mm_rag_cj_blog", embedding_function=OpenAIEmbeddings()
+)
+
+# Create retriever
+retriever_multi_vector_img = create_multi_vector_retriever(
+    vectorstore,
+    image_summaries,
+    img_base64_list,
+)
+
+
+def img_prompt_func(pages):
+    """
+    Join the context into a single string
+    """
+
+    messages = []
+
+    # Adding image(s) to the messages if present
+    for image in pages:
+        image_message = {
+                "type": "image_url",
+                "image_url": {"url": f"data:image/jpeg;base64,{image}"},
+        }
+    messages.append(image_message)
+
+    return [HumanMessage(content=messages)]
+
+def image_summarize(img_base64, prompt):
+    """Make image summary"""
+    chat = ChatOpenAI(model="gpt-4-vision-preview", max_tokens=1024)
+
+    msg = chat.invoke(
+        [
+            HumanMessage(
+                content=[
+                    {"type": "text", "text": prompt},
+                    {
+                        "type": "image_url",
+                        "image_url": {"url": f"data:image/jpeg;base64,{img_base64}"},
+                    },
+                ]
+            )
+        ]
+    )
+    return msg.content
+
+def generate_img_summaries(path):
+    """
+    Generate summaries and base64 encoded strings for images
+    path: Path to list of .jpg files extracted by Unstructured
+    """
+
+    # Store base64 encoded images
+    img_base64_list = []
+
+    # Store image summaries
+    image_summaries = []
+
+    # Prompt
+    prompt = """You are an assistant tasked with summarizing images for retrieval. \
+    These summaries will be embedded and used to retrieve the raw image. \
+    Give a concise summary of the image that is well optimized for retrieval."""
+
+    # Apply to images
+    for img_file in sorted(os.listdir(path)):
+        if img_file.endswith(".jpg"):
+            img_path = os.path.join(path, img_file)
+            base64_image = encode_image(img_path)
+            img_base64_list.append(base64_image)
+            image_summaries.append(image_summarize(base64_image, prompt))
+
+    return img_base64_list, image_summaries
+
+# Image summaries
+img_base64_list, image_summaries = generate_img_summaries(fpath)    
+
+def ask_ai(text, pages):
     _ = load_dotenv(find_dotenv()) # read local .env file
-    # client = OpenAI()
 
     print('ask_ai: starts')
+
     
     # response = retriever.invoke(text)
     
@@ -84,6 +178,11 @@ def ask_ai(text, retriever):
     print('prompt_template',  prompt_template)
   
     model = ChatOpenAI(api_key=os.environ.get("OPENAI_API_KEY"), model=OPENAI_MODEL)
+
+
+
+    response = model.invoke([message])
+    print(response.content)
 
     # Construction of the chain
     chain = (
@@ -157,8 +256,25 @@ def original_filename(history):
             file = line_part[0]
             print(" file1: ", file)
             return file
+
+def pdf_page_to_base64(pdf_document, page_number: int):
+    page = pdf_document.load_page(page_number)  # input is one-indexed
+    pix = page.get_pixmap()
+    img = Image.frombytes("RGB", [pix.width, pix.height], pix.samples)
+
+    buffer = io.BytesIO()
+    img.save(buffer, format="PNG")
+
+    return base64.b64encode(buffer.getvalue()).decode("utf-8")
        
 def load_pdfContent(filename):
+    doc = fitz.open(filename)
+    pages = []
+
+    for i in len(doc):
+        pages.add(pdf_page_to_base64(doc, i)) 
+
+    '''
     loader = PyPDFLoader(filename)
     pages = loader.load_and_split()
       
@@ -172,16 +288,17 @@ def load_pdfContent(filename):
             )
             pages = loader.load()
         
-            ''' 
+    
             for pageNum,imgBlob in enumerate(pages):
                 text = pytesseract.image_to_string(imgBlob,lang='eng')
 
                 with open(f'PDF_page.txt', 'w') as the_file:
                     the_file.write(text)
-            '''
+   
         except Exception as e:
             print(e)
-        
+    '''
+
     return pages
      
 def bot(history):
